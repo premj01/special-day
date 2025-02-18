acc9c046f1bf5fb58cea5e38d725e5ee1400d861
premj01 <150174529+premj01@users.noreply.github.com>
2025-02-18 23:40:45 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,9 @@
 var express = require('express');
 const mongoose = require('mongoose');
 var router = express.Router();
+require('dotenv').config();
 
-// mongoose.connect("mongodb+srv://freefiregaming012:jadhav@123@special-day.ipnyxls.mongodb.net/special-day?retryWrites=true&w=majority")
-
-mongoose.connect('mongodb+srv://freefiregaming012:jadhav%40123@special-day.ipnyxls.mongodb.net/special-day?retryWrites=true&w=majority')
+mongoose.connect(process.env.DBURL)
 
 const userSchema = mongoose.Schema({
   name: {
